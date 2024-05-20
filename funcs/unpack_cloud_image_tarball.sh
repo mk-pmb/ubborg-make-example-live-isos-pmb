f204e01531c3fe1cec59ffa80cae4cc36ce73d91
mk-pmb <mk-pmb@users.noreply.github.com>
2024-05-20 04:48:45 +0200

    unpack_cloud_image_tarball: Don't modify the $SECONDS.

--- funcs/unpack_cloud_image_tarball.sh
+++ funcs/unpack_cloud_image_tarball.sh
@@ -3,9 +3,9 @@
 
 function unpack_cloud_image_tarball () {
   local TGT_ROOT="${CFG[bread_chroot_path]:-/proc/ERROR/TGT_ROOT}"
-  mkdir --parents -- "$TGT_ROOT"
-  log_rtc_stopwatch 0 ': start.'
-  SECONDS=0
+  mkdir --parents -- "$TGT_ROOT" || return $?
+  log_rtc_stopwatch 0 ': unpack:'
+  local START=$SECONDS
   local UNP=(
     sudo
     tar
@@ -15,7 +15,7 @@ function unpack_cloud_image_tarball () {
     ${CFG[cloud_image_tar_opt]}
     )
   "${UNP[@]}" || return $?
-  log_rtc_stopwatch $SECONDS+1 ': done. took ≤ ¤ sec.'
+  log_rtc_stopwatch $SECONDS+1-$START ': done. took ≤ ¤ sec.'
 }
 
 
